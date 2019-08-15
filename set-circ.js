953eff71fd74156d07afd7f15b8fa2baaeb4c07a
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-15 20:21:58 -0300

    Movido para o lugar correto

---
+++ set-circ.js
@@ -0,0 +1,30 @@
+function arco()
+{
+    var angulo = parseFloat(prompt("Digite o valor do ângulo (em graus) do setor:", ""));
+    var raio = parseFloat(prompt("Digite o valor do raio:", ""));
+
+    var area = (angulo * raio * raio * 3,1415926)/360;
+    alert("O valor da área do setor circular (em função do arco) é: " +area);
+}
+function angulo()
+{
+    var comprimento = parseFloat(prompt("Digite o valor do comprimento do setor:", ""));
+    var raio = parseFloat(prompt("Digite o valor do raio:", ""));
+
+    var area = (comprimento * raio)/2;
+    alert("O valor da área do setor circular (em função do ângulo) é: " +area);
+}
+function p_circ()
+{
+    var raio = parseFloat(prompt("Digite o valor do raio:", ""));
+
+    var perimetro = 2 * 3,1415926 * raio;
+    alert("O valor do perímetro do círculo é: " +perimetro);
+}
+function a_circ()
+{
+    var raio = parseFloat(prompt("Digite o valor do raio:", ""));
+
+    var area = 3,1415926 * raio * raio;
+    alert("O valor da área do círculo é: " +area);
+}

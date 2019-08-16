3b493eae2b678c9bd547dc8c796cee648945d701
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-16 16:41:54 -0300

    Iniciando o front-end da parte de Conversão de Unidades

--- conversaoBases.js
+++ conversaoBases.js
@@ -14,11 +14,50 @@ function converter() {
     } else {
         alert("Selecione a base numérica de origem!");
     }
-    document.getElementById("explicacao").innerHTML = ("EXPLICAÇÃO DO CALCULO")
+    document.getElementById("explicacao").innerHTML = ("EXPLICAÇÃO DO CALCULO");
+}
+
+function ehDecimal (numero) {
+    var flag = 1;
+    for (i = 0; i < numero.length; i++) {
+        switch (numero[i]) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 0:
+                flag = 0;
+                break;
+            default:
+                flag = 1;
+                break;
+        }
+        if (flag == 1) {
+            return 0;
+        }
+    }
+
+    if (flag == 0) {
+        return 1;
+    }
 }
 
 function decimal(numero) {
+    var resultado = document.getElementById("resultado");
+    if (ehDecimal(numero) == 0) {
+        alert ("Digite um número válido!\n");
+        return;
+    }
+    if (document.forms[0].decimal.checked) {
+        alert ("A base de destino é igual à base de origem!");
+    } else {
 
+    }
 }
 
 function binaria(numero) {

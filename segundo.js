85370c32a1d97a8c33d710bcfca4800e8a9887ce
esterandr02 <48167967+esterandr02@users.noreply.github.com>
2019-08-26 17:26:09 -0300

    Comentado.

--- segundo.js
+++ segundo.js
@@ -9,11 +9,11 @@ function checagem(a, b, c) {
         } else { // descrições da equação do tipo se a < 0.
             document.getElementById("coeficientes").innerHTML = ("<br>Como na equação acima o <b>a</b> é negativo, seu gráfico será uma parábola com concavidade para <b>baixo</b>.");
         }
-        d(a, b, c);
+        d(a, b, c); // calcula o delta.
     }
 }
 
-function casa(num, cont) {
+function casa(num, cont) { // calcula o numero.
     cont = Math.pow(10, cont);
 
     return num * cont;
@@ -46,10 +46,10 @@ function digitos(n, digitado) { // contar a quantidade de digitos do a, b e c.
             }
         }
     }
-    gerar(conta, contb, contc, digitado, n, flag);
+    gerar(conta, contb, contc, digitado, n, flag);  // gera os digitos de acordo com os conts.
 }
 
-function converter(numero) {
+function converter(numero) { // Corrigi um um erro ao gerar o número.
     if (numero > 0) {
         return numero - 1;
     } else {
@@ -63,7 +63,11 @@ function gerar(conta, contb, contc, digitado, n, flag) { // gerar os termos(a, b
         c = 1,
         num;
     flag = 0;
-
+// O algoritmo para pegar os valores de a, b e c será o seguint:
+// Para a equação de formatdo ax² + bx + c = 0, tudo que vier antes de x² serão os dígitos que irão compor o "a", bem como tudo que vier antes
+// de x serão os dígitos que irão compor o "b", e por fim o que vier de resto serão os dígitos que irão compor o "c". 
+// OBS: neste algoritmo excluímos a contagem do + ou -, dentro dos ifs, e também sinalizamos se o numero é negativo a partir desses ifs. 
+   
     for (i = 0; i < n; i++) {
         if (digitado[i] == '-' || digitado[i] == '+') {
             if (digitado[i] == '-') {
@@ -116,8 +120,7 @@ function gerar(conta, contb, contc, digitado, n, flag) { // gerar os termos(a, b
             }
         }
     }
-    console.log(a, b, c);
-    checagem(a, b, c);
+    checagem(a, b, c); // Checa o delta e como será o gráfico dependendo dele.
 }
 
 function calculo() { // Irá pegar os valores dos coeficientes na expressão em formato texto.
@@ -142,15 +145,15 @@ function calculo() { // Irá pegar os valores dos coeficientes na expressão em 
 function d(a, b, c) {
     var delta = (b * b) - (4 * a * c);
 
-    if (delta > 0) {
+    if (delta > 0) { // descrição da equação para delta > 0.
         document.getElementById("delta").innerHTML = ("&Delta; = " + delta + ", logo, a equação possui duas raízes reais:");
         raiz(a, b, delta);
 
-    } else if (delta == 0) {
+    } else if (delta == 0) { // descrição da equação para delta = 0.
         document.getElementById("delta").innerHTML = ("&Delta; = " + delta + ", logo, a equação possui uma única raiz real:");
         raiz(a, b, delta);
 
-    } else { // delta menor que zero.
+    } else { // descrição da equação para delta < 0.
         document.getElementById("delta").innerHTML = ("&Delta; = " + delta + ", logo, a equação não possui raízes reais." + "Para <b>delta < 0</b>, a parábola não irá cortar o eixo x em nenhum ponto.");
         document.getElementById("raizes").innerHTML = ("(<i>x</i> &#8713; R)");
 
@@ -158,15 +161,16 @@ function d(a, b, c) {
 }
 
 function raiz(a, b, delta) { // Calcula a(s) raiz/ raizes da equacao, de acordo com o valor de delta.
-    var x1 = ((b * -1) + Math.sqrt(delta)) / (2 * a);
-    var x2 = ((b * -1) - Math.sqrt(delta)) / (2 * a);
+    var x1 = ((b * -1) + Math.sqrt(delta)) / (2 * a); // calcula x1.
+    var x2 = ((b * -1) - Math.sqrt(delta)) / (2 * a); // calcula x2.
 
-    x1 = parseFloat(x1);
-    x2 = parseFloat(x2);
+    x1 = parseFloat(x1); // cast de x1 para numero real.
+    x2 = parseFloat(x2); // cast de x2 para numero real.
 
-    x1 = x1.toFixed(4);
+    x1 = x1.toFixed(4); // formata o numero com 4 casas após a vírgula.
     x2 = x2.toFixed(4);
 
+    // Abaixo printa as descrições da equação e seu gráfico de acordo com o delta, e x1 e x2.
     document.getElementById("descricao1").innerHTML = (delta == 0) ? ("<i>x</i> = " + x1 + "Para <b>delta = 0</b>, a parábola irá cortar o <b>eixo x</b> no ponto " + x1 + ".") :
         ("<i>x'</i> = " + x1 + "</br><i>x''</i> = " + x2 + "<br><br>Para <b>delta > 0</b>, a parábola irá cortar o <b>eixo x</b> nos pontos " + x1 + " e " + x2 + ".");
 }

cd3f20796d53eb48ddd70c2895025aedf23eb62b
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-25 23:34:59 -0300

    Criando bom senso

--- conversaoBases.js
+++ conversaoBases.js
@@ -1,73 +1,33 @@
 function converter() {
-    var numero = document.forms[0].valor.value;
-    if (document.forms[0].decimalPara.checked) {
-        parseInt(numero);
-        decimal(numero);
-    } else if (document.forms[0].binariaPara.checked) {
-        parseInt(numero);
-        binaria(numero);
-    } else if (document.forms[0].hexadecimalPara.checked) {
-        hexadecimal(numero);
-    } else if (document.forms[0].octalPara.checked) {
-        parseInt(numero);
-        octal(numero);
-    } else {
-        alert("Selecione a base numérica de origem!");
-    }
-    document.getElementById("explicacao").innerHTML = ("EXPLICAÇÃO DO CALCULO");
-}
-
-function ehDecimal (numero) {
-    var flag = 1;
-    for (i = 0; i < numero.length; i++) {
-        switch (numero[i]) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 0:
-                flag = 0;
-                break;
-            default:
-                flag = 1;
-                break;
-        }
-        if (flag == 1) {
-            return 0;
-        }
-    }
-
-    if (flag == 0) {
-        return 1;
-    }
-}
-
-function decimal(numero) {
+    var n = parseFloat(document.forms[0].valor.value);
+    var origin = document.forms[0].origin.value;
+    var destiny = document.forms[0].destiny.value;
     var resultado = document.getElementById("resultado");
-    if (ehDecimal(numero) == 0) {
-        alert ("Digite um número válido!\n");
-        return;
+    var calc;
+    if (origin == 'Decimal') {
+        if (destiny == 'd') {
+            calc = n;
+            resultado.innerHTML = (n + "(decimal) é igual a " + calc + "(decimal)");
+        }
+        else {
+
+        }
     }
-    if (document.forms[0].decimal.checked) {
-        alert ("A base de destino é igual à base de origem!");
-    } else {
+    else {
+        if (destiny == 'b') {
+            calc = n;
+            resultado.innerHTML = (n + "(binário) é igual a " + calc + "(binário)");
+        }
+        else {
 
+        }
     }
 }
 
-function binaria(numero) {
+function decimalToBinaryu (n) {
 
 }
 
-function hexadecimal(numero) {
-
-}
-
-function octal(numero) {
-
+function binToDessemau (n) {
+    
 }
---
+++ massa.js
@@ -0,0 +1,7 @@
+function converter() {
+    var n = parseFloat(document.forms[0].valor.value);
+    var origin = document.forms[0].origin.value;
+    var destiny = document.forms[0].destiny.value;
+    var resultado = document.getElementById("resultado");
+    var calc;
+}

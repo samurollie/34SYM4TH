0c59dd6d376ba39d19ac768969c2bf3ac6e7c18f
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-26 15:30:51 -0300

    embelezando

--- conversaoBases.js
+++ conversaoBases.js
@@ -6,38 +6,35 @@ function converter() {
     var size = n.length;
     var origin = document.forms[0].origin.value;
     var destiny = document.forms[0].destiny.value;
-    n =  parseFloat(n);
+    n = parseFloat(n);
     if (origin == 'Decimal') {
         if (destiny == 'd') {
             calc = n;
             resultado.innerHTML = (n + "(decimal) é igual a " + calc + "(decimal)");
-        }
-        else {
-            decimalToBinaryu(n,size);
+        } else {
+            decimalToBinaryu(n, size);
             resultado.innerHTML = (n + "(decimal) é igual a " + calc.join('') + "(binário)");
 
         }
-    }
-    else {
+    } else {
         if (destiny == 'b') {
             calc = n;
             resultado.innerHTML = (n + "(binário) é igual a " + calc + "(binário)");
-        }
-        else {
-            calc = binToDessemau (n);
+        } else {
+            calc = binToDessemau(n);
             resultado.innerHTML = (n + "(binário) é igual a " + calc + "(decimal) ");
         }
     }
 }
 
-function decimalToBinaryu (n, size) {
-    calc = new Array (size);
+function decimalToBinaryu(n, size) {
+    calc = new Array(size);
     var i;
     for (i = 0; i < size; i++) {
         // console.log ("calc = " + calc.join('') + "i == " + i + "n == " + n);
         calc[i] = n % 2;
         if (n == 2) {
-            calc[i + 1] = n/2;
+            calc[i + 1] = n / 2;
         }
         n /= 2;
     }
@@ -46,12 +43,13 @@ function decimalToBinaryu (n, size) {
     return;
 }
 
-function binToDessemau (n) {
-    var resul = 0, i;
+function binToDessemau(n) {
+    var resul = 0,
+        i;
     for (i = 0; n >= 10; i++) {
         var a = n % 10;
         n /= 10;
-        resul += (a * Math.pow(2,i));
+        resul += (a * Math.pow(2, i));
     }
     resul += (n * Math.pow(2, i));
     return resul;

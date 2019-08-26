cfd9f5367f88e751ea2247f84c6f281c669fdce7
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-26 18:07:13 -0300

    Comentando as partes que faltaram

--- conversaoBases.js
+++ conversaoBases.js
@@ -1,49 +1,49 @@
-var calc;
+var calc; // Cria uma váriavel global que salva o resultado
 
 function converter() {
     var resultado = document.getElementById("resultado");
-    var n = document.forms[0].valor.value;
-    var size = n.length;
-    var origin = document.forms[0].origin.value;
-    var destiny = document.forms[0].destiny.value;
-    n = parseFloat(n);
+    var n = document.forms[0].valor.value; // Salva o valor digitado numa variável (até então uma string)
+    var size = n.length; // Salva o tamanho da string
+    var origin = document.forms[0].origin.value; // Salva a base de origen numa variável
+    var destiny = document.forms[0].destiny.value; // Salva a base de destino numa variável
+    n = parseFloat(n); // Transforma n num número flutuante
     if (origin == 'Decimal') {
-        if (destiny == 'd') {
+        if (destiny == 'd') { // Se eu desejo converter para decimal
             calc = n;
-            resultado.innerHTML = (n + "(decimal) é igual a " + calc + "(decimal)");
-        } else {
-            decimalToBinaryu(n, size);
-            resultado.innerHTML = (n + "(decimal) é igual a " + calc.join('') + "(binário)");
+            resultado.innerHTML = (n + "(decimal) é igual a " + calc + "(decimal)"); // Imprime
+        } else { // Se eu desejo converter para binária
+            decimalToBinaryu(n, size); // converte
+            resultado.innerHTML = (n + "(decimal) é igual a " + calc.join('') + "(binário)"); // Imprime 
 
         }
     } else {
-        if (destiny == 'b') {
+        if (destiny == 'b') { // Se eu desejo converter para binário
             calc = n;
-            resultado.innerHTML = (n + "(binário) é igual a " + calc + "(binário)");
-        } else {
-            calc = binToDessemau(n);
-            resultado.innerHTML = (n + "(binário) é igual a " + calc + "(decimal) ");
+            resultado.innerHTML = (n + "(binário) é igual a " + calc + "(binário)"); // Imprime
+        } else { // Se eu desejo converter para decimal
+            calc = binToDessemau(n); // Converte
+            resultado.innerHTML = (n + "(binário) é igual a " + calc + "(decimal) "); // Imprime
         }
     }
 }
 
-function decimalToBinaryu(n, size) {
-    calc = new Array(size);
+function decimalToBinaryu(n, size) { // Converte de decimal para binário;
+    calc = new Array(size); // Transforma a variável calc em um array;
     var i;
     for (i = 0; i < size; i++) {
         // console.log ("calc = " + calc.join('') + "i == " + i + "n == " + n);
-        calc[i] = n % 2;
+        calc[i] = n % 2; // Salva o valor da divisão por 2 no array;
         if (n == 2) {
             calc[i + 1] = n / 2;
         }
         n /= 2;
     }
     // console.log (calc);
-    calc.reverse();
+    calc.reverse(); // Inverte as posições do array;
     return;
 }
 
-function binToDessemau(n) {
+function binToDessemau(n) { // Transforma de binário para decimal
     var resul = 0,
         i;
     for (i = 0; n >= 10; i++) {
@@ -52,5 +52,5 @@ function binToDessemau(n) {
         resul += (a * Math.pow(2, i));
     }
     resul += (n * Math.pow(2, i));
-    return resul;
+    return resul; // retorna o resultado;
 }
